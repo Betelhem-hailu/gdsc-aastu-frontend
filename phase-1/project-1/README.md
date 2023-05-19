a4730556cd2be21b532366d4d32651b9419720f5
Betelhem-hailu <betelhemhailu811@gmail.com>
2023-05-19 16:14:10 +0300

    deploy

--- phase-1/project-1/README.md
+++
@@ -1,2 +0,0 @@
-Designing a Tribute Page using HTML & CSS 
-for A.P.J. Abdul Kalam is our first GDSC AASTU club study session project
--- phase-1/project-4/app.js
+++
@@ -1,9 +0,0 @@
-const colors = ['Blue','Yellow','Pink','Purple','Green','Red'];
-let change = document.getElementById('change');
-let box = document.getElementById('box');
-let text = document.getElementById('text');
-change.addEventListener('click',function(){
-    var randomC = colors[Math.floor(Math.random() * colors.length)]
-    box.style.backgroundColor = randomC
-    text.innerHTML = randomC
-})
